57dce290c623757102496fcf99439a7ab6c82d44
NaitikPatel <np03022005@gmail.com>
2023-10-16 18:49:50 +0530

    Bubble sort is working

--- Projects/Bubble_Sort_visulizer/script.js
+++ Projects/Bubble_Sort_visulizer/script.js
@@ -1,9 +1,81 @@
-function visualizer() {
-    var nums = document.getElementById("input").value;
-    const arr = nums.split(',').map(numStr => parseInt(numStr, 10));
-    // console.log(arr);
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function resetInput() {
+async function visualizer() {
+    var nums = document.getElementById("input").value;
+    const arr = nums.split(',').map(numStr => parseInt(numStr, 10));
+
+    var resultContainer = document.getElementById("result");
+
+    resultContainer.innerHTML = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        var elementBox = document.createElement("div");
+        elementBox.className = "array-element";
+        elementBox.textContent = arr[i];
+        resultContainer.appendChild(elementBox);
+    }
+}
+
+async function resetInput() {
     document.getElementById("input").value = "";
+    var resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = "";
+}
+
+async function bubbleSort(arr) {
+    var n = arr.length;
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+                updateVisualization(arr,i,i+1);
+                await sleep(3000); 
+            }
+        }
+    } while (swapped);
+    swappedarray(arr);
+}
+
+async function swappedarray(arr) {
+    var resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+        var elementBox = document.createElement("div");
+        elementBox.className = "array-element"; 
+        elementBox.textContent = arr[i];
+        elementBox.style.backgroundColor = "green"; 
+        resultContainer.appendChild(elementBox);
+    }
+}
+
+
+async function updateVisualization(arr,n,p) {
+    var resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+        var elementBox = document.createElement("div");
+        elementBox.className = "array-element";
+        elementBox.textContent = arr[i];
+        if(i==n || i==p){
+            elementBox.style.backgroundColor = "red"; 
+        }
+        else{
+            elementBox.style.backgroundColor = "lightblue"; 
+        }
+        resultContainer.appendChild(elementBox);
+    }
+}
+
+async function sortArray() {
+    var nums = document.getElementById("input").value;
+    const arr = nums.split(',').map(numStr => parseInt(numStr, 10));
+
+    await bubbleSort(arr);
 }
